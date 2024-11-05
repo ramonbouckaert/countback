dcf1a99b17727b184078ef0313262659ec275ac5
Ramon Bouckaert <ramon@bouckaert.com.au>
2024-11-05 23:34:39 +1100

    File loaders throw appropriate exception when file doesn't exist to support CI test hack

--- src/jsMain/kotlin/io/bouckaert/countback/JsFileLoader.kt
+++ src/jsMain/kotlin/io/bouckaert/countback/JsFileLoader.kt
@@ -9,6 +9,7 @@ import kotlinx.io.readLine
 class JsFileLoader: FileLoader {
     override suspend fun loadFile(path: String): Sequence<String> {
         val file = Path("../../../../$path")
+        if (!SystemFileSystem.exists(file)) throw FileLoader.Companion.FileLoadException("File at \"../../../../$path\" could not be found")
         val buffer = SystemFileSystem.source(file).buffered()
 
         return sequence {
--- src/jvmMain/kotlin/io/bouckaert/countback/JvmFileLoader.kt
+++ src/jvmMain/kotlin/io/bouckaert/countback/JvmFileLoader.kt
@@ -5,11 +5,14 @@ import kotlinx.coroutines.withContext
 import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.io.path.exists
 
 class JvmFileLoader: FileLoader {
     override suspend fun loadFile(path: String): Sequence<String> {
+        val file = Paths.get(path)
+        if (!file.exists()) throw FileLoader.Companion.FileLoadException("File at \"$path\" could not be found")
         return withContext(Dispatchers.IO) {
-            Files.newBufferedReader(Paths.get(path), Charset.forName("utf8")).lineSequence()
+            Files.newBufferedReader(file, Charset.forName("utf8")).lineSequence()
         }
     }
 }
